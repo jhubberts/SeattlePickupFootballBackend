4138331cfd27c6b922e6163e04d920454148ca25
John Hubberts <jhubberts@gmail.com>
2019-08-21 22:34:09 -0700

    RSVP will add you to the game if you're not in

--- src/rsvp.js
+++ src/rsvp.js
@@ -1,5 +1,7 @@
 import * as ResponseUtils from './utils/response-utils';
 import * as UserUtils from './utils/user-utils';
+import * as DateUtils from './utils/date-utils';
+import * as DAL from './dal';
 
 const rsvpHandler = async (event, context) => {
     try {
@@ -11,7 +13,19 @@ const rsvpHandler = async (event, context) => {
 
 const rsvp = async (event) => {
     const userData = await UserUtils.userDataForEvent(event);
-    console.log(userData);
+    let game = await DAL.getOrInitializeGame(DateUtils.isoDate(DateUtils.nextGameDay()));
+
+    const attendeeIndex = game.attendees.map(attendee => attendee.userPoolUserId).indexOf(userData.userPoolUserId);
+
+    if (attendeeIndex === -1) {
+        game.attendees.push({
+            userPoolUserId: userData.userPoolUserId,
+            identityId: userData.identityId,
+            name: userData.name,
+        });
+    }
+
+    await DAL.putGame(game);
 };
 
 export default rsvpHandler;
